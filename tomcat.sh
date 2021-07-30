73677e5368efdd45c123f6c0c5582a33fe181eec
Kishore Duggasani <79393479+kishoreduggasani@users.noreply.github.com>
2021-07-31 00:31:59 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -4,8 +4,8 @@
 sudo apt update -y
 
 #install java
-sudo apt install default-jre
+sudo apt install default-jre -y
 
 #install Tomcat
-sudo apt install tomcat8
+sudo apt install tomcat8 -y
 sudo systemctl start tomcat8
